4f8a046486b7932ed017a6a34e48db0a9f745222
Viswanadh <visureddy.v@gmail.com>
2014-06-20 09:28:06 -0500

    added boost serialization support

--- namedOptParam.h
+++ namedOptParam.h
@@ -4,14 +4,32 @@
 #include <iostream>
 #include <string>
 
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/string.hpp"
+
 class namedOptParam
 {
-public:
-  std::string description;
-  int idx;
-  float step;
-  std::string value;
-  std::string type;         /// single || range || set 
+
+    private:
+         friend class boost::serialization::access;
+        // When the class Archive corresponds to an output archive, the
+        // & operator is defined similar to <<.  Likewise, when the class Archive
+        // is a type of input archive the & operator is defined similar to >>.
+        template<class Archive>
+        void serialize(Archive &ar, const unsigned int version)
+        {
+            ar &description;
+            ar &idx;
+            ar &step;
+            ar &value;
+            ar &type;
+        }
+    public:
+        std::string description;
+        int idx;
+        float step;
+        std::string value;
+        std::string type;         /// single || range || set
 };
 
 
