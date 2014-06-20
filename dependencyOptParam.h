27c32ae3a9e6b598f862705ea56fb3b409808779
Viswanadh <visureddy.v@gmail.com>
2014-06-20 09:14:54 -0500

    added boost serialization support

--- dependencyOptParam.h
+++ dependencyOptParam.h
@@ -1,5 +1,23 @@
+#ifndef __DEPENDENCYOPTPARAM__
+#define __DEPENDENCYOPTPARAM__ 1
+
+#include <boost/serialization/string.hpp>
 class dependencyOptParam
 {
+    private:
+        friend class boost::serialization::access;
+        // When the class Archive corresponds to an output archive, the
+        // & operator is defined similar to <<.  Likewise, when the class Archive
+        // is a type of input archive the & operator is defined similar to >>.
+        template<class Archive>
+        void serialize(Archive &ar, const unsigned int version)
+        {
+            ar &variable_name;
+            ar &operand1;
+            ar &operand2;
+            ar &op;
+            ar &idx;
+        }
     public:
         std::string variable_name;
         std::string operand1;
@@ -7,3 +25,4 @@ class dependencyOptParam
         char op;
         int idx;       /// single || range || set
 };
+#endif
